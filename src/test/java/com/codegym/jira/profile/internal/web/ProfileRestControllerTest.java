166e51588868d14318be68b61f3d21cd3163d541
Enrique-LP <jesus902009@live.com>
2025-05-29 12:28:17 -0600

    Refactor ProfileRestControllerTest for improved readability and maintainability

--- src/test/java/com/codegym/jira/profile/internal/web/ProfileRestControllerTest.java
+++ src/test/java/com/codegym/jira/profile/internal/web/ProfileRestControllerTest.java
@@ -1,8 +1,103 @@
 package com.codegym.jira.profile.internal.web;
 
 import com.codegym.jira.AbstractControllerTest;
+import com.codegym.jira.MatcherFactory;
+import com.codegym.jira.common.util.JsonUtil;
+import com.codegym.jira.profile.ProfileTo;
+import com.codegym.jira.profile.internal.ProfileRepository;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static com.codegym.jira.profile.internal.web.ProfileTestData.*;
+
+@Slf4j
 class ProfileRestControllerTest extends AbstractControllerTest {
+    private static final String REST_URL = "/api/profile";
+    private static final MatcherFactory.Matcher<ProfileTo> PROFILE_TO_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(ProfileTo.class);
 
+    @Autowired
+    private ProfileRepository repository;
+
+    @Test
+    @WithUserDetails(value = "user@gmail.com")
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(PROFILE_TO_MATCHER.contentJson(USER_PROFILE_TO));
+    }
+
+    @Test
+    void getUnauthorized() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithUserDetails(value = "user@gmail.com")
+    void update() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(getUpdatedTo())))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        PROFILE_MATCHER.assertMatch(repository.getExisted(1L), getUpdated(1L));
+    }
+
+    @Test
+    @WithUserDetails(value = "user@gmail.com")
+    void updateInvalid() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(getInvalidTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = "user@gmail.com")
+    void updateWithUnknownNotification() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(getWithUnknownNotificationTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = "user@gmail.com")
+    void updateWithUnknownContact() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(getWithUnknownContactTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test
+    @WithUserDetails(value = "user@gmail.com")
+    void updateWithContactHtmlUnsafe() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(getWithContactHtmlUnsafeTo())))
+                .andDo(print())
+                .andExpect(status().isUnprocessableEntity());
+    }
+
+    @Test    void updateUnauthorized() throws Exception {
+        perform(MockMvcRequestBuilders.put(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(getUpdatedTo())))
+                .andExpect(status().isUnauthorized());
+    }
 }
